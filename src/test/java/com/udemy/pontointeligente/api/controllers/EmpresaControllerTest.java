33f65c7283b9cdc8341253ecf132cbaa29551720
Andrew <andrew.kretzer94@outlook.com>
2018-04-19 23:32:42 -0300

    Adicionado autenticacao via JWT

--- src/test/java/com/udemy/pontointeligente/api/controllers/EmpresaControllerTest.java
+++ src/test/java/com/udemy/pontointeligente/api/controllers/EmpresaControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -40,6 +41,7 @@ public class EmpresaControllerTest {
 	private static final String RAZAO_SOCIAL = "Empresa XYZ";
 
 	@Test
+	@WithMockUser
 	public void testBuscarEmpresaCnpjInvalido() throws Exception {
 		BDDMockito.given(this.empresaService.buscarPorCnpj(Mockito.anyString())).willReturn(Optional.empty());
 
@@ -49,6 +51,7 @@ public class EmpresaControllerTest {
 	}
 
 	@Test
+	@WithMockUser
 	public void testBuscarEmpresaCnpjValido() throws Exception {
 		BDDMockito.given(this.empresaService.buscarPorCnpj(Mockito.anyString()))
 				.willReturn(Optional.of(this.obterDadosEmpresa()));
--- src/test/java/com/udemy/pontointeligente/api/controllers/LancamentoControllerTest.java
+++ src/test/java/com/udemy/pontointeligente/api/controllers/LancamentoControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -38,62 +39,67 @@ public class LancamentoControllerTest {
 
 	@Autowired
 	private MockMvc mvc;
-	
+
 	@MockBean
 	private LancamentoService lancamentoService;
-	
+
 	@MockBean
 	private FuncionarioService funcionarioService;
-	
+
 	private static final String URL_BASE = "/api/lancamentos/";
 	private static final Long ID_FUNCIONARIO = 1L;
 	private static final Long ID_LANCAMENTO = 1L;
 	private static final String TIPO = TipoEnum.INICIO_TRABALHO.name();
 	private static final Date DATA = new Date();
-	
+
 	private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	
+
 	@Test
-	/*@WithMockUser*/
+	@WithMockUser
 	public void testCadastrarLancamento() throws Exception {
 		Lancamento lancamento = obterDadosLancamento();
-		BDDMockito.given(this.funcionarioService.buscarPorId(Mockito.anyLong())).willReturn(Optional.of(new Funcionario()));
+		BDDMockito.given(this.funcionarioService.buscarPorId(Mockito.anyLong()))
+				.willReturn(Optional.of(new Funcionario()));
 		BDDMockito.given(this.lancamentoService.persistir(Mockito.any(Lancamento.class))).willReturn(lancamento);
 
-		mvc.perform(MockMvcRequestBuilders.post(URL_BASE)
-				.content(this.obterJsonRequisicaoPost())
-				.contentType(MediaType.APPLICATION_JSON)
-				.accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.data.id").value(ID_LANCAMENTO))
-				.andExpect(jsonPath("$.data.tipo").value(TIPO))
+		mvc.perform(MockMvcRequestBuilders.post(URL_BASE).content(this.obterJsonRequisicaoPost())
+				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(jsonPath("$.data.id").value(ID_LANCAMENTO)).andExpect(jsonPath("$.data.tipo").value(TIPO))
 				.andExpect(jsonPath("$.data.data").value(this.dateFormat.format(DATA)))
 				.andExpect(jsonPath("$.data.funcionarioId").value(ID_FUNCIONARIO))
 				.andExpect(jsonPath("$.errors").isEmpty());
 	}
-	
+
 	@Test
-	/*@WithMockUser*/
+	@WithMockUser
 	public void testCadastrarLancamentoFuncionarioIdInvalido() throws Exception {
 		BDDMockito.given(this.funcionarioService.buscarPorId(Mockito.anyLong())).willReturn(Optional.empty());
 
-		mvc.perform(MockMvcRequestBuilders.post(URL_BASE)
-				.content(this.obterJsonRequisicaoPost())
-				.contentType(MediaType.APPLICATION_JSON)
-				.accept(MediaType.APPLICATION_JSON))
+		mvc.perform(MockMvcRequestBuilders.post(URL_BASE).content(this.obterJsonRequisicaoPost())
+				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isBadRequest())
 				.andExpect(jsonPath("$.errors").value("Funcionário não encontrado. ID inexistente."))
 				.andExpect(jsonPath("$.data").isEmpty());
 	}
-	
-	@Test
-	/*@WithMockUser(username = "admin@admin.com", roles = {"ADMIN"})*/
-	public void testRemoverLancamento() throws Exception {
-		BDDMockito.given(this.lancamentoService.buscarPorId(Mockito.anyLong())).willReturn(Optional.of(new Lancamento()));
 
-		mvc.perform(MockMvcRequestBuilders.delete(URL_BASE + ID_LANCAMENTO)
-				.accept(MediaType.APPLICATION_JSON))
+	@Test
+	@WithMockUser(username = "admin@admin.com", roles = { "ADMIN" })
+	public void testRemoverLancamento() throws Exception {
+		BDDMockito.given(this.lancamentoService.buscarPorId(Mockito.anyLong()))
+				.willReturn(Optional.of(new Lancamento()));
+
+		mvc.perform(MockMvcRequestBuilders.delete(URL_BASE + ID_LANCAMENTO).accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk());
+	}
+
+	@Test
+	@WithMockUser
+	public void testRemoverLancamentoAcessoNegado() throws Exception {
+		BDDMockito.given(this.lancamentoService.buscarPorId(Mockito.anyLong()))
+				.willReturn(Optional.of(new Lancamento()));
+
+		mvc.perform(MockMvcRequestBuilders.delete(URL_BASE + ID_LANCAMENTO).accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isForbidden());
 	}
 
 	private String obterJsonRequisicaoPost() throws JsonProcessingException {
@@ -114,6 +120,6 @@ public class LancamentoControllerTest {
 		lancamento.setFuncionario(new Funcionario());
 		lancamento.getFuncionario().setId(ID_FUNCIONARIO);
 		return lancamento;
-	}	
+	}
 
 }
